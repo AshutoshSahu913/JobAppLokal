c973b2ad084d00951103bc0db9e326007c001a41
Ashutosh <1ashutoshsahu@gmail>
2024-08-27 14:15:36 +0530

    clean code structure

--- app/src/main/java/com/example/jobapplokal/presentation_layer/MainActivity.kt
+++ app/src/main/java/com/example/jobapplokal/presentation_layer/MainActivity.kt
@@ -1,16 +1,12 @@
-package com.example.jobapplokal
+package com.example.jobapplokal.presentation_layer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.jobapplokal.ui.theme.JobAppLokalTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +17,7 @@ class MainActivity : ComponentActivity() {
             JobAppLokalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     innerPadding
-                    
+
                 }
             }
         }
